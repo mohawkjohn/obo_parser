469a8fa16ddaad03592a64c5c94df6d5ddb2285a
John Woods <john.woods@marcottelab.org>
2011-06-01 18:48:43 -0500

    Got a rudimentary graph working, documented it.

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,16 +1,76 @@
 require 'rubygems'
 require 'rgl/adjacency'
 
-# Graph of OBO terms.
+module RGL
+  class Edge::DirectedEdge
+    # Takes a source, target, and a reference to the terms in the OBO doc.
+    def initialize a, b, terms
+      raise(ArgumentError, "nil arguments") if a.nil? || b.nil?
+      @source, @target, @terms = a, b, terms
+    end
+
+    def to_s
+      ["(#{@source}[#{@terms[@source].id.value}]", relationships.join(','), "#{@target}[#{@terms[@target].id.value}])"].join('-')
+    end
+
+    # Get the source term (as opposed to the +source+). Note that +source+ returns the line number.
+    def source_term;  @terms[@source]; end
+
+    # Get the target term (as opposed to the +target+). Note that +target+ returns the line number.
+    def target_term;  @terms[@target]; end
+
+    # Give relationships between the OBO terms that make up this edge. Typically there is only one; if you're sure there
+    # is only one, use relationship.
+    def relationships
+      r = []
+      source_term.relationships.each do |rel|
+        r << rel.first if rel.last == target_term.id.value
+      end
+      target_term.relationships.each do |rel|
+        r << "~#{rel.first}" if rel.last == source_term.id.value
+      end
+      r
+    end
+
+    # Find the relationship that forms this edge. If there are multiple, no guarantee that the 'right' one will be
+    # returned.
+    def relationship
+      STDERR.puts("Warning: Multiple relationships detected for #{self.inspect}; returning first") if relationships.size > 1
+      relationships.first
+    end
+    
+  end
+end
+
+# Directed adjacency graph of OBO terms, based on RGL's DirectedAdjacencyGraph class.
 #
 # Ideally, should respond to all of the methods RGL::DirectedAdjacencyGraph responds to, but these have not all been
 # implemented.
 #
 # Also, under the OBO definition, some terms may have multiple vertices, so methods are not directly analogous. For
 # example, add_edge for two GO terms adds count(u)*count(v) edges, where count(x) is the number of stanzas for term x.
+#
+# Parse the Arabidopsis GO SLIM ontology:
+#   o               =  parse_obo_file(File.read("goslim_plant.obo"))
+#
+# Create graphs for different types of relationships
+#   graph           = {}
+#   graph[:is_a]    =  OboParser::Graph.new(o, :is_a)
+#   graph[:part_of] =  OboParser::Graph.new(o, :part_of)
+#
+# Combine the graphs
+#   graph[:all]     =  OboParser::Graph.new(o, :regulates, Set, graph[:is_a], graph[:part_of])
+#
+# Print out a table of source terms, target terms, and relationships between them, one relationship per line.
+#   graph.edges.each do |edge|
+#     edge.relationships.each do |rel|
+#       puts [edge.source_term.id.value, edge.target_term.id.value, rel].join("\t")
+#     end
+#   end
+#
 class OboParser::Graph < ::RGL::DirectedAdjacencyGraph
   def initialize obo, rtype = :is_a, edgelist_class = Set, *other_graphs
-    @relationship_type = rtype.to_s
+    rtype              = rtype.to_s
     @terms             = obo.terms
     term_id_to_indices = OboParser::Graph.hash_indices_by_term_id obo.terms
     edges              = []
@@ -21,9 +81,9 @@ class OboParser::Graph < ::RGL::DirectedAdjacencyGraph
     # Build edge list.
     @terms.each do |term|
       term.relationships.each do |rel|
-        if rel.first == @relationship_type
+        if rel.first == rtype
           hash_indices_by_term_id[rel.last].each do |term_index|
-            add_edge term_index, count
+            add_edge count, term_index
           end
         end
       end
@@ -41,13 +101,17 @@ class OboParser::Graph < ::RGL::DirectedAdjacencyGraph
     end
   end
 
-  
-  def inspect #:nodoc:
-    "#{@relationship_type.to_sym.inspect} #{super}"
+
+  # Returns an array of edges of the directed graph, but modified to include data on the terms that make up the nodes.
+  def edges
+    result = []
+    c = edge_class
+    each_edge { |u,v| result << c.new(u,v,@terms) }
+    result
   end
   
 
-  attr_reader :relationship_type, :terms
+  attr_reader :terms
 
 protected
 
@@ -69,6 +133,5 @@ protected
       h
     end
   end
-
 
 end
